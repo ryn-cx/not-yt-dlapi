39cc3bdeece20939a52f2d4b840cf293c3f64ef2
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2025-11-15 18:54:56 -0800

    Removed unused protocol component

--- src/not_yt_dlapi/protocol.py
+++ src/not_yt_dlapi/protocol.py
@@ -8,13 +8,6 @@ default_logger = logging.getLogger(__name__)
 
 
 class YTDLAPIProtocol(Protocol):
-    def _api_request(
-        self,
-        api_key: str,
-        *,
-        logger: logging.Logger = default_logger,
-    ) -> dict[str, Any]: ...
-
     def _parse_response[T: BaseModel](
         self,
         response_model: type[T],
