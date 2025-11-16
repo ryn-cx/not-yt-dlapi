4b3cdcf5c2153965df40b47a538d4e2e7cd362f5
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2025-11-16 00:50:39 -0800

    Improve model generation

--- src/not_yt_dlapi/update_files.py
+++ src/not_yt_dlapi/update_files.py
@@ -3,7 +3,6 @@ import uuid
 from typing import Any
 
 from gapi import (
-    INPUT_TYPE,
     GapiCustomizations,
     apply_customizations,
     update_json_schema_and_pydantic_model,
@@ -12,15 +11,12 @@ from gapi import (
 from not_yt_dlapi.constants import FILES_PATH, NOT_YT_DLAPI_PATH
 
 
-def update_model(
-    name: str,
-    data: INPUT_TYPE,
-    customizations: GapiCustomizations | None = None,
-) -> None:
+def update_model(name: str, customizations: GapiCustomizations | None = None) -> None:
     """Update a specific response model based on input data."""
     schema_path = NOT_YT_DLAPI_PATH / f"{name}/schema.json"
     model_path = NOT_YT_DLAPI_PATH / f"{name}/models.py"
-    update_json_schema_and_pydantic_model(data, schema_path, model_path, name)
+    files_path = FILES_PATH / name
+    update_json_schema_and_pydantic_model(files_path, schema_path, model_path, name)
     apply_customizations(model_path, customizations)
 
 
